4f4c188612f77b4a15d32bbbaae92f72afb0a4d4
Denis Obada <denis.o@linux.com>
2021-06-10 17:13:56 +0300

    Kelvin temperature source

---
+++ source-temp-kelvin/src/main/java/com/iot/source/pressure/KelvinTemperatureSource.java
@@ -0,0 +1,50 @@
+package com.iot.source.pressure;
+
+import com.iot.core.model.MetricValue;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class KelvinTemperatureSource {
+
+    @Autowired
+    private TaskScheduler taskScheduler;
+
+    @Autowired
+    private KafkaTemplate<String, MetricValue> kafkaTemplate;
+
+    @Value("${source.cron}")
+    private String sourceCron;
+
+    @Value("${source.flow}")
+    private List<String> defaultFlow;
+
+    @PostConstruct
+    void init() {
+        taskScheduler.schedule(() -> {
+            MetricValue metricValue = MetricValue.builder()
+                    .correlationId(UUID.randomUUID().toString())
+                    .sourceId("temperature-kelvin-sensor")
+                    .metricType("temperature")
+                    .metricName("kelvin-temperature-1")
+                    .flow(defaultFlow)
+                    .build();
+            metricValue.setValue(new Random().nextDouble());
+            String firstTopic = metricValue.firstFlowItem();
+            log.info("Sending message to topic {} {}", firstTopic, metricValue.getCorrelationId());
+            kafkaTemplate.send(firstTopic, new Date().toString(), metricValue);
+        }, new CronTrigger(sourceCron));
+    }
+
+}
